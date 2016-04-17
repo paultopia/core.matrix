00670b389db0b64b5c41e38661e8bc150a9c03da
Paul Gowder <paul.gowder@gmail.com>
2016-04-17 14:13:55 -0500

    intermediate commit as I muck with things

--- src/main/clojure/clojure/core/matrix/impl/pp2.clj
+++ src/main/clojure/clojure/core/matrix/impl/pp2.clj
@@ -1,9 +1,20 @@
 (ns clojure.core.matrix.impl.pp2
-  (:require [clojure.core.matrix.protocols :as mp])
+  (:require [clojure.core.matrix.protocols :as mp]
+    [clojure.core.matrix.impl.persistent-vector]
+    [clojure.core.matrix.impl.sequence]
+    [clojure.core.matrix.impl.defaults :as default]
+    [clojure.core.matrix.implementations :as imp :refer [*matrix-implementation*]]
+    [clojure.core.matrix.impl.mathsops :as mops]
+    [clojure.core.matrix.impl.wrappers :as wrap]
+    [clojure.core.matrix.utils :as u]
+    [clojure.core.matrix.impl.index]
+    [clojure.core.matrix.impl.pprint :as pprint]
+    [clojure.core.matrix.impl.double-array]
+    [clojure.core.matrix.impl.object-array])
   (:import [java.lang StringBuilder]
            [clojure.lang IPersistentVector]))
 
-(def cols [[1 20 300 4] [50 6000 77 8]])
+(def rows [[1 20 300 4] [50 6000 77 8]])
 
 (defn- format-num [x] (format "%.3f" (double x)))
 (defn- default-formatter [x]
@@ -32,3 +43,36 @@
         (partial string-helper formatter)
         {:max 0 :column []} %)
       vec-of-cols)))
+
+(defn twist
+  "take map output of stringer, rotate to rows + vector of col lengths"
+  [colmaps]
+  {:clen (mapv :max colmaps)
+    :rows (mp/transpose (mapv :column colmaps))})
+
+(defn- append-elem
+  "Appends an element, right-padding up to a given column length."
+  [^String elem ^long clen]
+  (let [c (long (count elem))
+        ws (- clen c)
+        sb (StringBuilder.)]
+    (dotimes [i ws]
+      (.append sb \space))
+    (.append sb elem)
+    (.toString sb)))
+
+(defn pad-row
+  [row clen-vec]
+  "takes a row of strings and a vector of column lengths and returns row as a padded string ending with newline"
+  )
+
+(defn pad-out [colmap]
+  (let [clen (:max colmap) col (:column colmap)]
+  (mapv #(append-elem % clen) col)))
+
+(defn matpad [vec-of-maps]
+  (mapv pad-out vec-of-maps))
+
+
+(defn brute-force [m]
+  (-> m mp/get-columns stringer matpad mp/transpose))
